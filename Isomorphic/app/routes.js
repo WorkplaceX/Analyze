70266a3678fe6287993a8fa8f76434326a109daf
WorkplaceX <danielsteiner772@yahoo.com>
2016-09-09 11:24:55 +0200

    Remove files generated by type script compiler.

--- Isomorphic/app/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var main_1 = require('./main');
-exports.routes = [
-    { path: '', redirectTo: 'home' },
-    { path: 'home', component: main_1.AppComponent },
-    { path: '**', redirectTo: 'home' }
-];
-//# sourceMappingURL=routes.js.map
