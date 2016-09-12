2715600dd3ca28b4ff8072ce0805f61b0d4f9567
WorkplaceX <danielsteiner772@yahoo.com>
2016-09-12 23:50:21 +0200

    Continuous Integration

---
+++ DataAccessLayer/ContinuousIntegration/ConnectionManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContinuousIntegration
+{
+    public class ConnectionManager
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                return @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Debug;Integrated Security=True";
+            }
+        }
+    }
+}
