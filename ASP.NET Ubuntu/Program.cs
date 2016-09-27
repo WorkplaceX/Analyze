78783a99b1a85a3116cda55c062bb9c37f9f4e21
WorkplaceX <danielsteiner772@yahoo.com>
2016-09-27 12:35:48 +0200

    ASP.NET running on Ubuntu accessing MySQL.

---
+++ ASP.NET Ubuntu/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace aspnetcoreapp
+{
+    using Microsoft.EntityFrameworkCore;
+    using MySQL.Data.EntityFrameworkCore.Extensions;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class MyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=127.0.0.1;userid=root;pwd=;database=My;sslmode=none;");
+        }
+
+        public DbSet<Person> Person { get; set; }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ ASP.NET Ubuntu/Startup.cs
@@ -0,0 +1,25 @@
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace aspnetcoreapp
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context =>
+            {
+                StringBuilder text = new StringBuilder();
+                var myContext = new MyContext();
+                foreach (var item in myContext.Person)
+                {
+                    text.AppendLine($"Name=" + item.Name + "; ");
+                }
+                //
+                return context.Response.WriteAsync("Hello from ASP.NET Core! " + text.ToString());
+            });
+        }
+    }
+}
