d9a7fcc37b8f7611ce72be55fc827494bbdc5ce7
WorkplaceX <danielsteiner772@yahoo.com>
2016-09-28 17:13:47 +0200

    ASP.NET Single Page Application

---
+++ ASP.NET (SPA)/WebApplication/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index.cshtml");
+        }
+    }
+}
