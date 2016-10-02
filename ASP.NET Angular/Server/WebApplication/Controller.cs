45ffaaafbbd07d8d8d4f9caed9bf4039ca83a406
WorkplaceX <danielsteiner772@yahoo.com>
2016-10-02 20:00:57 +0200

    Server init

---
+++ ASP.NET Angular/Server/WebApplication/Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication
+{
+    public class HomeController : Controller
+    {
+        [Route("/Home")]
+        public IActionResult Index()
+        {
+            return View("Home.cshtml");
+        }
+    }
+
+    public class JavaScriptController : Controller
+    {
+        [Route("/JavaScript")]
+        public IActionResult Index()
+        {
+            return View("JavaScript.cshtml");
+        }
+    }
+}
---
+++ ASP.NET Angular/Server/WebApplication/wwwroot/Debug.js
@@ -0,0 +1,9 @@
+module.exports = function (params) {
+    return new Promise(function (resolve, reject) {
+        var result = '<h1>Hello world from Server Side JavaScript!</h1>'
+           + '<p>Current time in Node.js is: ' + new Date() + '</p>'
+           + '<p>Request path is: ' + params.location.path + '</p>'
+           + '<p>Absolute URL is: ' + params.absoluteUrl + '</p>';
+        resolve({ html: result });
+    });
+};
