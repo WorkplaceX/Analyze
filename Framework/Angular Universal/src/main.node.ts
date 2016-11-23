98ea2771f7d2271c958128fcc9dbc54c5c94c694
WorkplaceX <danielsteiner772@yahoo.com>
2016-11-23 13:53:13 +0100

    Framework

---
+++ Framework/Angular Universal/src/main.node.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { UniversalModule } from 'angular2-universal';
+
+import { App, Home } from './app/app';
+
+@NgModule({
+  bootstrap: [ App ],
+  declarations: [ App, Home ],
+  imports: [
+    UniversalModule, // NodeModule, NodeHttpModule, and NodeJsonpModule are included
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: Home, pathMatch: 'full' }
+    ])
+  ]
+})
+export class MainModule {
+
+}
---
+++ Framework/Angular/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { DataService, Data } from './dataService';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <h1>Angular2 App with Bootstrap</h1>
+      </div>
+    </div>    
+    <div class="row">
+      <div class="col-sm-4">
+        <p>Data=({{ data.Name }})</p>
+      </div>
+      <div class="col-sm-4">
+        Second of three columns
+        <button class="btn btn-primary" (click)="click($event)">Click</button>
+      </div>
+      <div class="col-sm-4">
+        Third of three columns
+        <label class="btn btn-primary" >
+          <input type="checkbox" /> Button
+        </label>
+      </div>
+    </div>
+  </div>  
+`,
+  providers: [DataService]  
+})
+export class AppComponent { 
+  data: Data;
+
+  constructor(dataService: DataService){
+    this.data = dataService.data;
+  }
+
+  click(event){
+    this.data.Name += ".";
+  }
+}
+
