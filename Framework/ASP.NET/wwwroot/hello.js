f623d92cb2bfd87419a488286d0749bc97a3910c
WorkplaceX <danielsteiner772@yahoo.com>
2016-12-02 18:37:11 +0100

    IISNode on server.

---
+++ Framework/ASP.NET/wwwroot/hello.js
@@ -0,0 +1,6 @@
+﻿var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, world! Node version: ' + process.version);
+}).listen(process.env.PORT);
