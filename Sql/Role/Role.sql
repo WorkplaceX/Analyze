8e1c65ae96149375c15cbebdc9fc146b2f567d85
WorkplaceX <danielsteiner772@yahoo.com>
2016-12-14 23:11:27 +0100

    Update for role sql.

--- Sql/Role/Role.sql
+++ Sql/Role/Role.sql
@@ -1,5 +1,6 @@
-IF EXISTS(SELECT * FROM sys.views WHERE name = 'SyRoleView') DROP VIEW SyRoleView
-IF EXISTS(SELECT * FROM sys.views WHERE name = 'LoRoleView') DROP VIEW LoRoleView
+IF EXISTS(SELECT * FROM sys.views WHERE name = 'LoRoleMatrix') DROP VIEW LoRoleMatrix
+IF EXISTS(SELECT * FROM sys.views WHERE name = 'SyRoleAccess') DROP VIEW SyRoleAccess
+IF EXISTS(SELECT * FROM sys.views WHERE name = 'LoRoleAccess') DROP VIEW LoRoleAccess
 IF EXISTS(SELECT * FROM sys.tables WHERE name = 'LoRoleUser') DROP TABLE LoRoleUser
 IF EXISTS(SELECT * FROM sys.tables WHERE name = 'LoRoleLoation') DROP TABLE LoRoleLoation
 IF EXISTS(SELECT * FROM sys.tables WHERE name = 'LoRole') DROP TABLE LoRole
@@ -20,6 +21,8 @@ INSERT INTO SyUser(Name)
 SELECT 'Max'
 UNION ALL
 SELECT 'Fritz'
+UNION ALL
+SELECT 'Heinz'
 
 /* SyRole */
 CREATE TABLE SyRole
@@ -37,8 +40,8 @@ SELECT 'Guest', 0
 CREATE TABLE SyRoleUser
 (
 	Id INT PRIMARY KEY IDENTITY,
-	UserId INT NOT NULL FOREIGN KEY REFERENCES SyUser(Id),
-	RoleId INT NOT NULL FOREIGN KEY REFERENCES SyRoleUser(Id),
+	UserId INT NOT NULL FOREIGN KEY REFERENCES SyUser(Id), /* Source */
+	RoleId INT NOT NULL FOREIGN KEY REFERENCES SyRoleUser(Id), /* Dest */
 	IsActive BIT NOT NULL,
 	UNIQUE (UserId, RoleId)
 )
@@ -74,16 +77,16 @@ SELECT 'German', 0
 UNION ALL
 SELECT 'French', 0
 UNION ALL
-SELECT 'Admin Loation', 1
+SELECT 'Admin Module', 1
 
 
 /* LoRoleLoation */
 CREATE TABLE LoRoleLoation
 (
 	Id INT PRIMARY KEY IDENTITY,
-	RoleId INT FOREIGN KEY REFERENCES LoRole(Id),
-	UserId INT FOREIGN KEY REFERENCES SyUser(Id), /* Link user directly to location */
-	LoationId INT NOT NULL FOREIGN KEY REFERENCES LoLoation(Id),
+	RoleId INT FOREIGN KEY REFERENCES LoRole(Id), /* Source */
+	UserId INT FOREIGN KEY REFERENCES SyUser(Id), /* Source (link user directly to location) */
+	LoationId INT NOT NULL FOREIGN KEY REFERENCES LoLoation(Id), /* Dest */
 	IsActive BIT NOT NULL,
 	UNIQUE (RoleId, UserId, LoationId)
 )
@@ -100,8 +103,8 @@ SELECT NULL, (SELECT Id FROM SyUser WHERE Name = 'Fritz'), (SELECT Id FROM LoLoa
 CREATE TABLE LoRoleUser
 (
 	Id INT PRIMARY KEY IDENTITY,
-	UserId INT NOT NULL FOREIGN KEY REFERENCES SyUser(Id),
-	RoleId INT NOT NULL FOREIGN KEY REFERENCES LoRole(Id),
+	UserId INT NOT NULL FOREIGN KEY REFERENCES SyUser(Id), /* Source */
+	RoleId INT NOT NULL FOREIGN KEY REFERENCES LoRole(Id), /* Dest */
 	IsActive BIT NOT NULL,
 	UNIQUE (UserId, RoleId)
 )
@@ -109,25 +112,31 @@ INSERT INTO LoRoleUser (UserId, RoleId, IsActive)
 SELECT Id, (SELECT Id FROM LoRole WHERE Name = 'German'), 1 FROM SyUser WHERE Name = 'Max'
 UNION ALL
 SELECT Id, (SELECT Id FROM LoRole WHERE Name = 'French'), 1 FROM SyUser WHERE Name = 'Fritz'
+UNION ALL
+SELECT Id, (SELECT Id FROM LoRole WHERE Name = 'Admin Module'), 1 FROM SyUser WHERE Name = 'Heinz'
 
 GO
 
-CREATE VIEW SyRoleView AS
+CREATE VIEW SyRoleAccess AS
 SELECT 
 	SyUser.Id AS UserId,
 	SyUser.Name AS UserName,
 	SyRole.Id AS RoleId,
-	SyRole.Name AS RoleName
+	SyRole.Name AS RoleName,
+	SyRole.IsAdmin AS IsAdmin
 
 FROM
 	SyUser SyUser
-	
-CROSS JOIN
-	SyRole SyRole
 
+LEFT JOIN
+	SyRoleUser SyRoleUser ON (SyRoleUser.UserId = SyUser.Id AND SyRoleUser.IsActive = 1)
+
+LEFT JOIN
+	SyRole SyRole ON (SyRole.Id = SyRoleUser.RoleId)
+	
 GO	
 
-CREATE VIEW LoRoleView AS
+CREATE VIEW LoRoleAccess AS
 SELECT 
 	SyUser.Id AS UserId,
 	SyUser.Name AS UserName,
@@ -138,33 +147,54 @@ FROM
 	SyUser SyUser
 	
 LEFT JOIN
-	SyRoleUser SyRoleUser ON (SyRoleUser.UserId = SyUser.Id)
+	SyRoleUser SyRoleUser ON (SyRoleUser.UserId = SyUser.Id AND SyRoleUser.IsActive = 1)
 
 LEFT JOIN
 	SyRole SyRole ON (SyRole.Id = SyRoleUser.RoleId)
 	
 LEFT JOIN
-	LoRoleUser LoRoleUser ON (LoRoleUser.UserId = SyUser.Id)
+	LoRoleUser LoRoleUser ON (LoRoleUser.UserId = SyUser.Id AND LoRoleUser.IsActive = 1)
 	
 LEFT JOIN
 	LoRole LoRole ON (LoRole.Id = LoRoleUser.RoleId)
 	
 LEFT JOIN
-	LoRoleLoation LoRoleLoation ON (LoRoleLoation.RoleId = LoRoleUser.RoleId OR LoRoleLoation.UserId = SyUser.Id)
+	LoRoleLoation LoRoleLoation ON ((LoRoleLoation.RoleId = LoRoleUser.RoleId OR LoRoleLoation.UserId = SyUser.Id) AND LoRoleLoation.IsActive = 1)
 	
 LEFT JOIN
 	LoLoation LoLoation ON (LoLoation.Id = LoRoleLoation.LoationId OR SyRole.IsAdmin = 1 OR LoRole.IsAdmin = 1)
-	
-WHERE
-	SyRoleUser.IsActive = 1 AND
-	LoRoleUser.IsActive = 1 AND
-	LoRoleLoation.IsActive = 1
-	
+
 GROUP BY
 	SyUser.Id,
 	SyUser.Name,
 	LoLoation.Id,
 	LoLoation.Name
 
+GO
+
+CREATE VIEW LoRoleMatrix AS
+SELECT 
+	SyUser.Id AS UserId,
+	SyUser.Name AS UserName,
+	LoLoation.Id AS LoationId,
+	LoLoation.Name AS LoationName,
+	LoRole.Id AS RoleId,
+	LoRole.Name AS RoleName,
+	(SELECT LoRoleUser.IsActive FROM LoRoleUser WHERE LoRoleUser.UserId = SyUser.Id AND LoRoleUser.RoleId = LoRole.Id) AS IsRole,
+	(SELECT LoRoleLoation.IsActive FROM LoRoleLoation LoRoleLoation WHERE LoRoleLoation.UserId = SyUser.Id AND LoRoleLoation.LoationId = LoLoation.Id) AS IsDirect,
+	(SELECT SyRoleAccess.IsAdmin FROM SyRoleAccess SyRoleAccess WHERE SyRoleAccess.UserId = SyUser.Id) AS IsAdmin,
+	(SELECT LoRoleUser.IsActive FROM LoRoleUser LoRoleUser WHERE LoRoleUser.UserId = SyUser.Id AND LoRoleUser.RoleId = (SELECT LoRole.Id FROM LoRole LoRole WHERE LoRole.IsAdmin = 1)) AS IsAdminModule,
+	(SELECT 1 FROM LoRoleAccess LoRoleAccess WHERE LoRoleAccess.UserId = SyUser.Id AND LoRoleAccess.LoationId = LoLoation.Id) AS IsAccess
+
+FROM
+	SyUser SyUser
+
+CROSS JOIN
+	LoLoation LoLoation
+
+CROSS JOIN
+	LoRole LoRole
+
 GO	
-SELECT * FROM LoRoleView
+
+SELECT * FROM LoRoleMatrix
