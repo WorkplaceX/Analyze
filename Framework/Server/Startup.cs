32d63e37e6d97b393bfb89375bf21a7fc8006ebc
WorkplaceX <danielsteiner772@yahoo.com>
2017-01-12 23:20:23 +0100

    Update to .NET Core 1.1

--- Framework/Server/Startup.cs
+++ Framework/Server/Startup.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
 
 namespace Server
 {
