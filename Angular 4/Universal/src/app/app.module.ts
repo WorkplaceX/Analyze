a8b428a96afd3cf0719947bb7a91001970bbef23
WorkplaceX <info@workplacex.com>
2017-06-22 11:48:36 +0200

    Universal

---
+++ Angular 4/Universal/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({appId: 'Universal'})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
