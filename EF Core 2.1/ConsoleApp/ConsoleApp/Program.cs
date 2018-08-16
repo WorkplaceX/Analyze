28bc7c8c2dbd5b38182285391225b57b0bea44f0
WorkplaceX <info@workplacex.com>
2018-08-16 14:13:44 +0200

    EF 2.1

---
+++ EF Core 2.1/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Select
+            My my = Query<My>().Where(item => item.Id == 1).ToList().First();
+
+            // Update
+            var dbContext = DbContext();
+            My myUpdate = new My() { Id = my.Id, Text = my.Text };
+            myUpdate.Text += ".";
+            dbContext.Attach(my).CurrentValues.SetValues(myUpdate);
+            dbContext.SaveChanges();
+
+            // Insert
+            My myInsert = new My() { Text = "New" };
+            dbContext.Add(myInsert);
+            dbContext.SaveChanges();
+
+            // Select View
+            List<MyView> myViewList = Query<MyView>().ToList();
+            var myView = myViewList.First();
+
+            // Update View
+            myUpdate = new My() { Id = myView.Id, Text = myView.Text };
+            myUpdate.Text += "V";
+            dbContext.Attach(myView).CurrentValues.SetValues(myUpdate);
+            dbContext.SaveChanges();
+
+        }
+
+        public static DbContext DbContext()
+        {
+            var typeMappingSource = new SqlServerTypeMappingSource(new TypeMappingSourceDependencies(new ValueConverterSelector(new ValueConverterSelectorDependencies())), new RelationalTypeMappingSourceDependencies()); // EF Core 2.1
+
+            var conventionSet = SqlServerConventionSetBuilder.Build();
+            var builder = new ModelBuilder(conventionSet);
+
+            // My
+            var entity = builder.Entity(typeof(My));
+            entity.ToTable("My");
+            entity.Property("Id");
+            entity.Property("Id").HasAnnotation(CoreAnnotationNames.TypeMapping, typeMappingSource.FindMapping(typeof(int)));
+            entity.Property("Text").HasAnnotation(CoreAnnotationNames.TypeMapping, typeMappingSource.FindMapping(typeof(string)));
+
+            // MyView
+            entity = builder.Entity(typeof(MyView));
+            entity.ToTable("MyView");
+            entity.Property("Id");
+            entity.Property("Id").HasAnnotation(CoreAnnotationNames.TypeMapping, typeMappingSource.FindMapping(typeof(int)));
+            entity.Property("Text").HasAnnotation(CoreAnnotationNames.TypeMapping, typeMappingSource.FindMapping(typeof(string)));
+
+            var model = builder.Model;
+            var options = new DbContextOptionsBuilder();
+            options.UseSqlServer("Data Source=localhost; Initial Catalog=AdventureWorks2016; Integrated Security=True;").UseModel(model);
+
+            var dbContext = new DbContext(options.Options);
+            return dbContext;
+        }
+
+        public static IQueryable<T> Query<T>()
+        {
+            var dbContext = DbContext();
+            IQueryable query = (IQueryable)(dbContext.GetType().GetTypeInfo().GetMethod("Set").MakeGenericMethod(typeof(T)).Invoke(dbContext, new object[] { }));
+            return (IQueryable<T>)query;
+        }
+    }
+    public class My
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+    }
+
+    public class MyView
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+    }
+}
---
+++ EF Core 2.1/Readme.md
@@ -0,0 +1,6 @@
+# EF Core 2.1
+Build model programmatically and perform select, update and insert operation.
+
+## Packages
+* Microsoft.EntityFrameworkCore (2.1.1)
+* Microsoft.EntityFrameworkCore.SqlServer (2.1.1)
