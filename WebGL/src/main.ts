7bcfd1d598cfddbdb3139468608ada466b3a5bfe
WorkplaceX <info@workplacex.com>
2019-05-28 22:52:17 +0200

    Rotation

--- WebGL/src/main.ts
+++ WebGL/src/main.ts
@@ -33,15 +33,19 @@ class WebGL {
   program: WebGLProgram;
 
   vertexShaderSource: string = `
-    attribute vec3 vertPosition; // IN
-    attribute vec3 vertColor; // IN
-    varying vec3 fragColor; // OUT
-    void main() {
-      fragColor = vertColor;
-      gl_Position = vec4(vertPosition, 1.0); // (X, Y, Z, W) W is used for clipping.
-    }`;
+  #version 100 // https://en.wikipedia.org/wiki/OpenGL_Shading_Language; https://caniuse.com/#search=webgl;
+  attribute vec3 vertPosition; // IN
+  uniform mat4 uniformMatrix; // IN
+  attribute vec3 vertColor; // IN
+  varying vec3 fragColor; // OUT
+  void main() {
+    gl_Position = uniformMatrix * vec4(vertPosition, 0); // (X, Y, Z, W) W is used for clipping cube (+Z, -Z and -X, +X and -Y, +Y).
+    gl_Position.w = 1.0;
+    fragColor = vertColor;
+  }`;
 
   fragmentShaderSource: string = `
+  #version 100 // https://en.wikipedia.org/wiki/OpenGL_Shading_Language; https://caniuse.com/#search=webgl;
   precision mediump float;
   #define resolution vec2(640.0, 480.0)
   #define thickness 0.003
@@ -68,19 +72,19 @@ class WebGL {
   uniform vec4 uniformColor; // IN Global
   uniform sampler2D uniformTexture; // IN Global
   void main() {
-      vec2 texcoord = vec2(gl_FragCoord.x / resolution.x, gl_FragCoord.y / resolution.y);  // get a value from the middle of the texture
-      if (texture2D(uniformTexture, (vec2(0, 0))) == vec4(1, 1, 1, 0)) // Texture not yet loaded. See also: Uint8Array([255, 255, 255, 0])
-      {
-        gl_FragColor = vec4(fragColor, 1); // Color defined in vertex.
-      }
-      else
-      {
-        gl_FragColor = gl_FragColor + texture2D(uniformTexture, texcoord); // Color from texture.
-      }
-      gl_FragColor = gl_FragColor + vec4(drawLine(vec2(0.6, 0.3), vec2(0.6, 0.5))); // Draw line.
-      gl_FragColor = gl_FragColor * uniformColor; // Color defined global.
-      // gl_FragColor = vec4(drawLine(vec2(0.6, 0.3), vec2(0.6, 0.5))) + texture2D(uniformTexture, texcoord) * uniformColor;
-    }`;
+    vec2 texcoord = vec2(gl_FragCoord.x / resolution.x, gl_FragCoord.y / resolution.y);  // get a value from the middle of the texture
+    if (texture2D(uniformTexture, (vec2(0, 0))) == vec4(1, 1, 1, 0)) // Texture not yet loaded. See also: Uint8Array([255, 255, 255, 0])
+    {
+      gl_FragColor = vec4(fragColor, 1); // Color defined in vertex.
+    }
+    else
+    {
+      gl_FragColor = gl_FragColor + texture2D(uniformTexture, texcoord); // Color from texture.
+    }
+    gl_FragColor = gl_FragColor + vec4(drawLine(vec2(0.6, 0.3), vec2(0.6, 0.5))); // Draw line.
+    gl_FragColor = gl_FragColor * uniformColor; // Color defined global.
+    // gl_FragColor = vec4(drawLine(vec2(0.6, 0.3), vec2(0.6, 0.5))) + texture2D(uniformTexture, texcoord) * uniformColor;
+  }`;
 
   initShader(): void {
     var vertexShader = this.createShader(this.gl, this.vertexShaderSource, this.gl.VERTEX_SHADER);
@@ -111,9 +115,9 @@ class WebGL {
       -0.2, +0.9, +0.0, 0, 1, 0,
       -0.9, +0.9, +0.0, 0, 0, 1,
 
-      -0.9, +0.2, +1.0, 1, 0, 0,
-      +0.5, +0.2, -0.2, 0, 1, 0,
-      -0.2, +0.9, +1.0, 0, 0, 1,
+      -0.9, +0.2, +0.01, 1, 0, 0,
+      +0.5, +0.2, -0.0018, 0, 1, 0,
+      -0.2, +0.9, +0.01, 0, 0, 1,
     ])
 
     var buffer = this.gl.createBuffer();
@@ -189,10 +193,101 @@ class WebGL {
     var uniformTexture = this.gl.getUniformLocation(this.program, 'uniformTexture');
     this.gl.uniform1i(uniformTexture, 7); // this.gl.TEXTURE7
 
+    // GLSL parameter "uniformMatrix"
+    var uniformMatrix = this.gl.getUniformLocation(this.program, 'uniformMatrix');
+    // var mat4: Float32List = Mat4.projection(2, 1, 1);
+    // mat4 = Mat4.translate(mat4, 2, 2, 0);
+    var mat4 = Mat4.xRotation((time as any / 4000 % 1) * Math.PI * 2);
+    this.gl.uniformMatrix4fv(uniformMatrix, false, mat4);
+
     this.gl.drawArrays(this.gl.TRIANGLES, 0,
-      this.vertex.length / (this.vertexElementCount as any)); // Number of vertex.
+    this.vertex.length / (this.vertexElementCount as any)); // Number of vertex.
 
     requestAnimationFrame(this.mainLoop);
+  }
+}
+
+class Mat4 {
+  static multiply(a: Float32List, b: Float32List): Float32List {
+    var a00 = a[0 * 4 + 0];
+    var a01 = a[0 * 4 + 1];
+    var a02 = a[0 * 4 + 2];
+    var a03 = a[0 * 4 + 3];
+    var a10 = a[1 * 4 + 0];
+    var a11 = a[1 * 4 + 1];
+    var a12 = a[1 * 4 + 2];
+    var a13 = a[1 * 4 + 3];
+    var a20 = a[2 * 4 + 0];
+    var a21 = a[2 * 4 + 1];
+    var a22 = a[2 * 4 + 2];
+    var a23 = a[2 * 4 + 3];
+    var a30 = a[3 * 4 + 0];
+    var a31 = a[3 * 4 + 1];
+    var a32 = a[3 * 4 + 2];
+    var a33 = a[3 * 4 + 3];
+    var b00 = b[0 * 4 + 0];
+    var b01 = b[0 * 4 + 1];
+    var b02 = b[0 * 4 + 2];
+    var b03 = b[0 * 4 + 3];
+    var b10 = b[1 * 4 + 0];
+    var b11 = b[1 * 4 + 1];
+    var b12 = b[1 * 4 + 2];
+    var b13 = b[1 * 4 + 3];
+    var b20 = b[2 * 4 + 0];
+    var b21 = b[2 * 4 + 1];
+    var b22 = b[2 * 4 + 2];
+    var b23 = b[2 * 4 + 3];
+    var b30 = b[3 * 4 + 0];
+    var b31 = b[3 * 4 + 1];
+    var b32 = b[3 * 4 + 2];
+    var b33 = b[3 * 4 + 3];
+    return [
+      b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+      b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+      b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+      b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+      b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+      b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+      b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+      b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+      b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+      b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+      b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+      b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+      b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+      b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+      b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+      b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
+    ];
+  }
+
+  static translation(tx: number, ty: number, tz:number): Float32List {
+    return [
+       1,  0,  0,  0,
+       0,  1,  0,  0,
+       0,  0,  1,  0,
+       tx, ty, tz, 1,
+    ];
+  }
+
+  static xRotation(angleInRadians: number): Float32List {
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    return [
+      1, 0, 0, 0,
+      0, c, s, 0,
+      0, -s, c, 0,
+      0, 0, 0, 1,
+    ];
+  }
+
+  static translate(m: Float32List, tx: number, ty: number, tz: number) {
+    return Mat4.multiply(m, Mat4.translation(tx, ty, tz));
+  }
+
+  static xRotate(m: Float32List, angleInRadians: number) {
+    return Mat4.multiply(m, Mat4.xRotation(angleInRadians));
   }
 }
 
