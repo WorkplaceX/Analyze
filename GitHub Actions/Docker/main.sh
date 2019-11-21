d49823db3e86897a78dbd406795be61fd5fb25ee
WorkplaceX <info@workplacex.com>
2019-11-21 16:36:19 +0100

    Docker

--- GitHub Actions/Docker/main.sh
+++ GitHub Actions/Docker/main.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -l
-echo "Hello from Docker"
-lsb_release -a
+
+set -x # Enable print execute cammands to stdout.
+BASH_XTRACEFD=1 # Print execute command to stdout. Not to stderr.
+
+echo "Hello from Docker 2"
+cat /etc/os-release
