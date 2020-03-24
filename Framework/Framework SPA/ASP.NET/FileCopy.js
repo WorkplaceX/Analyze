f0de5b831de491ed3dfe9e65fd62f25023e94a64
WorkplaceX <info@workplacex.com>
2020-03-24 18:11:10 +0100

    Remove Framework

--- Framework/Framework SPA/ASP.NET/FileCopy.js
+++
@@ -1,3 +0,0 @@
-var fs = require('fs');
-
-fs.createReadStream('../Angular Universal/dist/server/index.js').pipe(fs.createWriteStream('Application/Nodejs/Server/AngularUniversalServer.js'));
--- Framework/Framework/Build.cmd
+++
@@ -1,4 +0,0 @@
-ECHO .NET Core 1.1 https://www.microsoft.com/net/download/core
-CD Submodule/Build
-dotnet restore
-dotnet run
