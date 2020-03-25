ad2d6876f442375c48e65961b6da507368662760
WorkplaceX <info@workplacex.com>
2020-03-25 16:46:54 +0100

    Move to Util

--- Sql/Pivot.sql
+++
@@ -1,4 +0,0 @@
-/* SQL Pivot */
-SELECT User_SK, FieldName, Value
-FROM [Database].[dboDatabase].[User]
-UNPIVOT (Value FOR FieldName in (email, firstname)) AS Contact
--- Youtube/Readme.md
+++
@@ -1,2 +0,0 @@
-# Screen Cast
-Best resolution: https://support.google.com/youtube/answer/6375112
