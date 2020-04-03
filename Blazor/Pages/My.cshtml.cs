fff48eb0a3bfa2ce7508153570fdfc0b6757104c
WorkplaceX <info@workplacex.com>
2020-04-03 20:21:10 +0200

    Blazor

---
+++ Blazor/Pages/My.cshtml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorApp.Pages
+{
+    public class MyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void X()
+        {
+
+        }
+    }
+}
