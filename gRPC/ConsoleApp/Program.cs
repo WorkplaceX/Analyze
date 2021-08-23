2e38c633d81495e1877386dcdb70d7aea78ed9bb
WorkplaceX <info@workplacex.com>
2021-08-23 10:16:00 +0200

    Service and Client

---
+++ gRPC/ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // HTTP/2
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ gRPC/Readme.md
@@ -0,0 +1,5 @@
+# Create
+* Create a new project ASP.NET Core gRPC Service (Uses NuGet package Grpc.AspNetCore)
+* Add a new project Console Application
+* Manage Connected Services add service reference gRPC
+* Select client to be generated
